ff5f986a2f8cce5389ac9f3adca4f2668d5ebbaf
Stuart Chaney <stuartchaney22@gmail.com>
2011-07-06 21:47:32 -0700

    all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stuartdyra::Application.config.secret_token = '4aa4e59982acbd5c9cd866f0f7de6947577e6ee22e5d07295ca30e847e0a7bfbc03675f9f48491b4ef547dfa2ddcdc62fe626f78cf32e6f826e4d6d43b22914a'
